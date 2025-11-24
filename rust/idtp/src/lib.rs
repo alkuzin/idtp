53e5715e50bb0f9cdf9f1254ad68e98eb506b411
nnulldev <(null)>
2025-11-24 11:00:46 +0300

    [feat]: added Idtp struct

--- rust/idtp/src/lib.rs
+++ rust/idtp/src/lib.rs
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0.
 // Copyright (C) 2025-present idtp project and contributors.
 
-//! Inertial Measurement Unit Data Transfer Protocol (IDTP) implementation.
+//! Inertial Measurement Unit Data Transfer Protocol (IDTP) - network protocol
+//! used for transfering IMU data. This protocol is suitable for usage in areas
+//! of robotics, unmanned vehicles, wearable devices and etc.
 //!
-//! This crate was designed for usage on embedded systems.
+//! This crate was designed for use on `embedded systems`.
 
 #![no_std]
+// Ignore #[must_use] suggestions from clippy.
+#![allow(clippy::must_use_candidate)]
 
 mod header;
+mod idtp;
 pub use header::*;
+pub use idtp::*;
