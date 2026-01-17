3c5515cfa4c8a413712f4bfa3ab117d3d26f163c
Alexander <alkuzindev@gmail.com>
2026-01-17 12:45:30 +0300

    [feat]: added IdtpError enum & IdtpResult alias

--- src/rust/idtp/src/lib.rs
+++ src/rust/idtp/src/lib.rs
@@ -3,7 +3,7 @@
 
 //! Inertial Measurement Unit Data Transfer Protocol (IDTP) - network protocol
 //! used for transferring IMU data. This protocol is suitable for usage in areas
-//! of robotics, unmanned vehicles, wearable devices and etc.
+//! of robotics, unmanned vehicles, wearable devices etc.
 //!
 //! This crate was designed for use on `embedded systems`.
 
@@ -11,7 +11,28 @@
 // Ignore #[must_use] suggestions from clippy.
 #![allow(clippy::must_use_candidate)]
 
+#[cfg(feature = "software_impl")]
+pub mod crypto;
 mod header;
 mod idtp;
+
 pub use header::*;
 pub use idtp::*;
+
+/// Protocol errors enumeration.
+#[derive(Debug)]
+pub enum IdtpError {
+    /// Buffer too short.
+    BufferUnderflow,
+    /// Incorrect CRC value.
+    InvalidCrc,
+    /// Incorrect HMAC value.
+    InvalidHMac,
+    /// Incorrect HMAC key.
+    InvalidHMacKey,
+    /// Error to convert from/to bytes.
+    ParseError,
+}
+
+/// Result alias for IDTP.
+pub type IdtpResult<T> = Result<T, IdtpError>;
