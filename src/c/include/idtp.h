303dc0da262a52be7929e54693cc5fb349c15bf3
Alexander <alkuzindev@gmail.com>
2026-01-26 15:04:48 +0300

    [feat]: rewrited the rest of functions and declarations for IDTP v2

--- src/c/include/idtp.h
+++ src/c/include/idtp.h
@@ -7,13 +7,13 @@
 
 #ifndef IDTP_H
 #define IDTP_H
-#include <iterator>
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
 #include <stdint.h>
+#include <string.h>
 
 // Utils section.
 
@@ -35,7 +35,7 @@ enum IdtpResult {
 
 // Cryptographic functions section.
 
-#ifndef SOFTWARE_IMPL
+#ifdef SOFTWARE_IMPL
 
 /// @brief Calculate software-based `CRC-8`.
 ///
@@ -136,6 +136,8 @@ typedef struct {
 /// Size of IDTP header in bytes.
 const size_t IDTP_HEADER_SIZE = sizeof(IdtpHeader);
 
+// Frame section.
+
 /// IDTP frame max size in bytes. It includes size of IDTP header,
 /// payload and packet trailer.
 const size_t IDTP_FRAME_MAX_SIZE = 1024;
@@ -160,13 +162,10 @@ typedef struct {
 }
 #endif // __cplusplus
 
-#endif IDTP_H
-
+#endif // IDTP_H
 
 // Implementation section.
 #ifdef IDTP_IMPLEMENTATION
-
-#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -179,6 +178,23 @@ void idtp_create_header(IdtpHeader *header) {
     memset(header, 0, IDTP_HEADER_SIZE);
     header->preamble = IDTP_PREAMBLE;
     header->version = IDTP_VERSION;
+}
+
+/// @brief Get frame trailer size.
+///
+/// @param [in] header given IDTP header to handle.
+/// @return Trailer size in bytes.
+inline size_t idtp_header_trailer_size(const IdtpHeader* header) {
+    switch (header->mode) {
+        case IDTP_MODE_LITE:
+            return 0;
+        case IDTP_MODE_SAFETY:
+            return 4;
+        case IDTP_MODE_SECURE:
+            return 32;
+        default:
+            return 0;
+    }
 }
 
 /// @brief Construct new IDTP frame.
@@ -213,45 +229,6 @@ IdtpResult idtp_frame_set_payload(
     frame->payload_size = payload_size;
     frame->header.payload_size = (uint16_t)payload_size;
 
-    return IDTP_RESULT_OK;
-}
-
-/// @brief Get frame trailer size.
-///
-/// @return Trailer size in bytes.
-inline size_t idtp_frame_trailer_size(const IdtpFrame* frame) {
-    switch (frame->header.mode) {
-        case IDTP_MODE_LITE:
-            return 0;
-        case IDTP_MODE_SAFETY:
-            return 4;
-        case IDTP_MODE_SECURE:
-            return 32;
-        default:
-            return 0;
-    }
-}
-
-
-
-    IdtpResult sw_crc8(const uint8_t *data, size_t size, uint8_t *out) {
-    // TODO:
-    return IDTP_RESULT_OK;
-}
-
-    IdtpResult sw_crc32(const uint8_t *data, size_t size, uint8_t *out) {
-    // TODO:
-    return IDTP_RESULT_OK;
-}
-
-    IdtpResult sw_hmac(
-        const uint8_t *data,
-        size_t size,
-        const uint8_t *key,
-        size_t key_size,
-        uint8_t *out
-    ) {
-    // TODO:
     return IDTP_RESULT_OK;
 }
 
@@ -328,7 +305,7 @@ IdtpResult idtp_frame_pack_with(
     size_t *frame_size
 ) {
     const size_t payload_size = frame->payload_size;
-    const size_t trailer_size = idtp_frame_trailer_size(frame);
+    const size_t trailer_size = idtp_header_trailer_size(&frame->header);
     const size_t expected_size = IDTP_FRAME_MIN_SIZE + payload_size + trailer_size;
     *frame_size = 0;
 
@@ -339,7 +316,7 @@ IdtpResult idtp_frame_pack_with(
     // Packing IDTP header & calculating the CRC-8.
     const size_t header_size = IDTP_HEADER_SIZE;
     memcpy(buffer, &frame->header, IDTP_HEADER_SIZE);
-    calc_crc8(buffer, 19, &buffer[19]);
+    calc_crc8(buffer, 18, &buffer[19]);
 
     // Packing payload.
     memcpy(buffer + IDTP_HEADER_SIZE, frame->payload, payload_size);
@@ -349,21 +326,138 @@ IdtpResult idtp_frame_pack_with(
 
     switch (frame->header.mode) {
         case IDTP_MODE_LITE:
-            return;
+            break;
         case IDTP_MODE_SAFETY:
             calc_crc32(buffer, data_size, buffer + data_size);
-            return;
+            break;
         case IDTP_MODE_SECURE:
             calc_hmac(buffer, data_size, key, key_size, buffer + data_size);
-            return;
+            break;
         default:
-            return;
+            break;
     }
 
     *frame_size = data_size + trailer_size;
+    return IDTP_RESULT_OK;
 }
 
-#ifndef SOFTWARE_IMPL
+/// @brief Validate IDTP frame integrity with custom `CRC` and `HMAC` calculation.
+/// Recommended to use if hardware acceleration for `CRC`/`HMAC` available.
+///
+/// @param [in] frame given IDTP frame object to handle.
+/// @param [out] buffer given buffer to store IDTP frame bytes.
+/// @param [in] buffer_size given buffer size in bytes.
+/// @param [in] calc_crc8 given function with custom `CRC-8` calculation logic.
+/// @param [in] calc_crc32 given function with custom `CRC-32` calculation logic.
+/// @param [in] calc_hmac given function with custom `HMAC-SHA256` calculation logic.
+/// @param [in] key given `HMAC` key.
+/// @param [in] key_size given `HMAC` key size in bytes.
+///
+/// @return IDTP_RESULT_OK - in case of success
+/// @return Error otherwise.
+IdtpResult idtp_frame_validate_with(
+    const IdtpFrame *frame,
+    uint8_t *buffer,
+    const size_t buffer_size,
+    calc_crc8_fn calc_crc8,
+    calc_crc32_fn calc_crc32,
+    calc_hmac_fn calc_hmac,
+    const uint8_t *key,
+    size_t key_size
+) {
+    const size_t header_size = IDTP_HEADER_SIZE;
+
+    if (buffer_size < header_size)
+        return IDTP_RESULT_BUFFER_UNDERFLOW;
+
+    // Checking CRC-8 of IDTP header.
+    const uint8_t received_crc8 = buffer[19];
+    uint8_t computed_crc8 = 0;
+    calc_crc8(buffer, 18, &computed_crc8);
+
+    if (received_crc8 != computed_crc8)
+        return IDTP_RESULT_INVALID_CRC;
+
+    // Checking size.
+    const IdtpHeader *header = (const IdtpHeader*)buffer;
+
+    const size_t payload_size = header->payload_size;
+    const size_t trailer_size = idtp_header_trailer_size(header);
+    const size_t data_size = header_size + payload_size;
+    const size_t expected_size = data_size + trailer_size;
+
+    if (buffer_size < expected_size)
+        return IDTP_RESULT_BUFFER_UNDERFLOW;
+
+    // Checking frame trailer.
+    switch (frame->header.mode) {
+        case IDTP_MODE_LITE:
+            break;
+
+        case IDTP_MODE_SAFETY: {
+            uint32_t computed_crc32 = 0;
+            calc_crc32(buffer, data_size, (uint8_t*)&computed_crc32);
+
+            uint32_t received_crc32 = *(buffer + data_size);
+
+            if (computed_crc32 != received_crc32)
+                return IDTP_RESULT_INVALID_CRC;
+            break;
+        }
+
+        case IDTP_MODE_SECURE: {
+            uint8_t computed_hmac[32] = {};
+            calc_hmac(buffer, data_size, key, key_size, (uint8_t*)&computed_hmac);
+
+            const uint8_t *received_hmac = buffer + data_size;
+
+            if (memcmp(received_hmac, computed_hmac, 32) != 0)
+                return IDTP_RESULT_INVALID_HMAC;
+
+            break;
+        }
+        default:
+            return IDTP_RESULT_INVALID_CRC;
+    }
+
+    return IDTP_RESULT_OK;
+}
+
+/// @brief Convert byte slice into IDTP frame.
+///
+/// @param [in] buffer given buffer to handle.
+/// @param [in] size given size of the buffer in bytes.
+/// @param [out] out given frame to hold the result.
+///
+/// @return IDTP frame struct from byte slice - in case of success.
+/// @return Error otherwise.
+IdtpResult idtp_frame_from_bytes(const uint8_t *buffer, size_t size, IdtpFrame *out) {
+    const size_t header_size = IDTP_HEADER_SIZE;
+
+    if (size < header_size)
+        return IDTP_RESULT_BUFFER_UNDERFLOW;
+
+    IdtpHeader *header = (IdtpHeader*)(buffer + header_size);
+
+    IdtpFrame frame = {};
+    idtp_create_frame(&frame);
+
+    frame.header = *header;
+    frame.payload_size = header->payload_size;
+
+    const size_t trailer_size = idtp_header_trailer_size(header);
+    const size_t expected_size = header_size + frame.payload_size + trailer_size;
+
+    if (size < expected_size)
+        return IDTP_RESULT_BUFFER_UNDERFLOW;
+
+    memcpy(frame.payload, buffer + header_size, frame.payload_size);
+    *out = frame;
+
+    return IDTP_RESULT_OK;
+}
+
+#ifdef SOFTWARE_IMPL
 /// @brief Pack into raw IDTP frame.
 /// `CRC` & `HMAC` calculation is software-based.
 ///
@@ -395,9 +489,43 @@ IdtpResult idtp_frame_pack(
         key_size,
         frame_size
     );
-}
-#endif // SOFTWARE_IMPL
 
+    return IDTP_RESULT_OK;
+}
+
+/// @brief Validate IDTP frame integrity. `CRC` & `HMAC` calculation
+/// is software-based.
+///
+/// @param [in] frame given IDTP frame object to handle.
+/// @param [out] buffer given buffer to store IDTP frame bytes.
+/// @param [in] buffer_size given buffer size in bytes.
+/// @param [in] key given `HMAC` key.
+/// @param [in] key_size given `HMAC` key size in bytes.
+///
+/// @return Frame size in bytes - in case of success.
+/// @return Error otherwise.
+IdtpResult idtp_frame_validate(
+    const IdtpFrame *frame,
+    uint8_t *buffer,
+    const size_t buffer_size,
+    const uint8_t *key,
+    size_t key_size
+) {
+    idtp_frame_validate_with(
+        frame,
+        buffer,
+        buffer_size,
+        sw_crc8,
+        sw_crc32,
+        sw_hmac,
+        key,
+        key_size
+    );
+
+    return IDTP_RESULT_OK;
+}
+
+#endif // SOFTWARE_IMPL
 
 #ifdef __cplusplus
 }
