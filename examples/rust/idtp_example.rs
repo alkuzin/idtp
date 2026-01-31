636a19a99c1692d13113d97390ef8bde6ec60923
Alexander <alkuzindev@gmail.com>
2026-01-31 13:25:06 +0300

    [refactor]: updated idtp crate usage example to IDTP v2.1.0 version

--- examples/rust/idtp_example.rs
+++ examples/rust/idtp_example.rs
@@ -1,68 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0.
 // Copyright (C) 2025-present idtp project and contributors.
 
-//! IDTP v2.0.0 usage example.
+//! IDTP v2.1.0 usage example.
 
-use idtp::{IdtpFrame, IdtpHeader, Mode};
-use std::{mem, process};
-
-/// Example IDTP payload struct.
-#[derive(Debug, Default, Clone, Copy)]
-#[repr(C, packed)]
-pub struct ImuPayload {
-    pub acc_x: f32,
-    pub acc_y: f32,
-    pub acc_z: f32,
-    pub gyr_x: f32,
-    pub gyr_y: f32,
-    pub gyr_z: f32,
-}
-
-/// Example payload size in bytes.
-pub const PAYLOAD_SIZE: usize = size_of::<ImuPayload>();
-
-impl ImuPayload {
-    /// Convert payload to bytes.
-    ///
-    /// # Returns
-    /// - Payload byte array.
-    pub fn as_bytes(&self) -> [u8; PAYLOAD_SIZE] {
-        unsafe { mem::transmute::<Self, [u8; PAYLOAD_SIZE]>(*self) }
-    }
-
-    /// Convert a byte slice to a `Payload` struct.
-    ///
-    /// # Parameters
-    /// - `bytes` - given bytes to convert.
-    ///
-    /// # Returns
-    /// - Payload from bytes.
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        let mut payload = Self::default();
-
-        unsafe {
-            std::ptr::copy_nonoverlapping(
-                bytes.as_ptr(),
-                &mut payload as *mut Self as *mut u8,
-                size_of::<Self>(),
-            );
-        }
-        payload
-    }
-}
+use idtp::{
+    IdtpFrame, IdtpHeader, Mode,
+    payload::{IdtpPayload, Imu3Acc, Imu3Gyr, Imu6},
+};
+use std::process;
 
 fn main() {
     // -----------------------------------------------------------------------
     // 1) SENDER SIDE: Creating and packing an IDTP v2 frame.
     // -----------------------------------------------------------------------
 
-    let imu_data = ImuPayload {
-        acc_x: 0.001,
-        acc_y: 0.002,
-        acc_z: 0.003,
-        gyr_x: 0.004,
-        gyr_y: 0.005,
-        gyr_z: 0.006,
+    let imu_data = Imu6 {
+        acc: Imu3Acc {
+            acc_x: 0.001,
+            acc_y: 0.002,
+            acc_z: 0.003,
+        },
+        gyr: Imu3Gyr {
+            gyr_x: 0.004,
+            gyr_y: 0.005,
+            gyr_z: 0.006,
+        },
     };
 
     let mut frame = IdtpFrame::new();
@@ -76,7 +38,7 @@ fn main() {
     // Important: In v2, set_header should be called before set_payload
     // or payload size must be synchronized.
     frame.set_header(&header);
-    let _ = frame.set_payload(&imu_data.as_bytes());
+    let _ = frame.set_payload(&imu_data);
 
     // Prepare buffer for data transmission.
     let mut buffer = [0u8; 64];
@@ -122,7 +84,7 @@ fn main() {
     // Extract and use data.
     let header = decoded_frame.header();
     let payload_bytes = decoded_frame.payload().unwrap();
-    let payload = ImuPayload::from_bytes(payload_bytes);
+    let payload = Imu6::from_bytes(payload_bytes);
 
     println!("Received header: {:#?}", header);
     println!("Received payload: {:#?}", payload);
